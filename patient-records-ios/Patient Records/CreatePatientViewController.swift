ec19892dbaa10285f64eefbd40886637531c1237
valinl1 <ljv1996@gmail.com>
2016-10-02 22:57:29 -0500

    Add new patient page

--- patient-records-ios/Patient Records/CreatePatientViewController.swift
+++ patient-records-ios/Patient Records/CreatePatientViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class CreatePatientViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let viewSize = view.layer.bounds
+        scrollView.contentSize = CGSize(width: viewSize.width, height: 1000)
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +24,5 @@ class CreatePatientViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
---
+++ patient-records-ios/Patient Records/NestableUIView.swift
@@ -0,0 +1,22 @@
+
+//
+//  NestableUIVIew.swift
+//  Patient Records
+//
+//  Created by Lukas Valine on 10/2/16.
+//  Copyright © 2016 Lukas Valinevaline. All rights reserved.
+//
+
+import UIKit
+
+class NestableUIView: UIView {
+
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        for subview in subviews as [UIView] {
+            if !subview.isHidden && subview.alpha > 0 && subview.isUserInteractionEnabled && subview.point(inside: convert(point, to: subview), with: event) {
+                return true
+            }
+        }
+        return false
+    }
+}
