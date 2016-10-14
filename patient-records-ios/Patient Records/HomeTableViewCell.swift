fba6397ace8301cd9666813eb368e2194e1a4725
valinl1 <ljv1996@gmail.com>
2016-10-14 13:14:12 -0500

    Added automated icons

---
+++ patient-records-ios/Patient Records/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Patient Records
+//
+//  Created by Lukas Valine on 10/11/16.
+//  Copyright © 2016 Lukas Valinevaline. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ patient-records-ios/Patient Records/LogoView.swift
@@ -0,0 +1,21 @@
+//
+//  LogoView.swift
+//  Patient Records
+//
+//  Created by Lukas Valine on 10/12/16.
+//  Copyright © 2016 Lukas Valinevaline. All rights reserved.
+//
+
+import UIKit
+
+class LogoView: SKView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
